b76e238d36bfbdebf0ee4f1aa6f43c290e600523
Antti Mikkonen <a.mikkonen@iki.fi>
2018-01-15 16:16:26 +0200

    library support test

---
+++ exampleCode/testLibrarySupport.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 15 16:08:36 2018
+
+@author: ami
+"""
+
+from CoolProp.CoolProp import PropsSI
+import openpyxl
+import scipy 
+import matplotlib
+
+print("OK")
+
+
